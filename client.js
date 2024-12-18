16bf487f0700f13dc87bbd178c03008c3f01eed8
thatao72 <tsuyoshi.hatao@gmail.com>
2024-12-18 09:36:03 +0900

    move computation out to .gs

--- client.js
+++ client.js
@@ -8,26 +8,7 @@ function displayData(weeklyData) {
       const tableData = [];
       for (const sundayStr in targetData.weeks) {
         const week = targetData.weeks[sundayStr];
-
-        let activityBubbles = "";
-        if (week.runDistance > 0) activityBubbles += '<span class="bubble run">Run: ' + week.runDistance.toFixed(2) + 'km, ' + week.runTimeFormatted + '</span>';
-        if (week.bikeDistance > 0) activityBubbles += '<span class="bubble bike">Bike: ' + week.bikeDistance.toFixed(2) + 'km, ' + week.bikeTimeFormatted + '</span>';
-        if (week.bikeIndoorPower > 0) activityBubbles += '<span class="bubble bike">Bike Indoor: ' + week.bikeIndoorPower.toFixed(0) + 'W, ' + week.bikeTimeFormatted + '</span>';
-        if (week.swimDistance > 0) activityBubbles += '<span class="bubble swim">Swim: ' + week.swimDistance.toFixed(2) + 'km, ' + week.swimTimeFormatted + '</span>';
-
-        let eventBubbles = "";
-        if (Array.isArray(week.events)) {
-          week.events.forEach(event => {
-            eventBubbles += '<span class="bubble event">' + event + '</span>';
-          });
-        }
-
-        tableData.push({
-          sunday: week.sunday, // Directly use the string
-          countdown: week.countdown,
-          activity: activityBubbles,
-          events: eventBubbles
-        });
+        tableData.push(week.tableRow);
       }
       
       new Tabulator("#table-" + safeTargetDateId, {
