9c2361cd943e7896fbdf2493d11f639957cf4dff
thatao72 <tsuyoshi.hatao@gmail.com>
2024-12-18 11:16:01 +0900

    add failure catch

--- client.js
+++ client.js
@@ -1,16 +1,19 @@
-console.log("client.js is loaded!");
-const weeklyData = google.script.run.withSuccessHandler(displayData).getWeeklyData();
-console.log("weeklyData is ready!");
+google.script.run
+  .withSuccessHandler(displayData)
+  .withFailueHandler(onFailure)
+  .getWeeklyData();
 
 function displayData(weeklyData) {
+  console.log("displayData called!");
   window.addEventListener('DOMContentLoaded', function() {
+    console.log("window load event");
     for (const targetDateStr in weeklyData) {
+      console.log("Inside for loop");
       const safeTargetDateId = String(targetDateStr).replace(/\//g, '-');
       const targetData = weeklyData[targetDateStr];
       const tableData = [];
       for (const sundayStr in targetData.weeks) {
         const week = targetData.weeks[sundayStr];
-        console.log("week contents is taken " + week.tableRow)
         tableData.push(week.tableRow);
       }
       
@@ -26,4 +29,10 @@ function displayData(weeklyData) {
       });
     }
   });
+}
+
+function onFailure(error) {
+  console.error("Error from server:", error); // Log the error
+  // Optionally display an error message to the user
+  alert("An error occurred. Please check the console.");
 }
