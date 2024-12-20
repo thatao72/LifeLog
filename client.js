19576bbb7398add29bf02f7b578f393d3cab375b
thatao72 <tsuyoshi.hatao@gmail.com>
2024-12-20 18:33:34 +0900

    indoor bike average power number digit fix

--- client.js
+++ client.js
@@ -35,7 +35,7 @@ function displayData(weeklyData) {
             const bikeIndoorPower = data.bikeIndoorTime !== 0 ? data.bikeIndoorPower.toFixed(0)/data.bikeIndoorTime : 10000;
             if (data.runDistance > 0) activityBubbles += `<span class="bubble run">Run: ${data.runDistance.toFixed(2)}km, ${formatTime(data.runTime)}</span>`;
             if (data.bikeDistance > 0) activityBubbles += `<span class="bubble bike">Bike: ${data.bikeDistance.toFixed(2)}km, ${formatTime(data.bikeTime)}</span>`;
-            if (data.bikeIndoorPower > 0) activityBubbles += `<span class="bubble bike">Bike Indoor: ${bikeIndoorPower}W, ${formatTime(data.bikeIndoorTime)}</span>`;
+            if (data.bikeIndoorPower > 0) activityBubbles += `<span class="bubble bike">Bike Indoor: ${bikeIndoorPower.toFixed(2)}W, ${formatTime(data.bikeIndoorTime)}</span>`;
             if (data.swimDistance > 0) activityBubbles += `<span class="bubble swim">Swim: ${data.swimDistance.toFixed(2)}km, ${formatTime(data.swimTime)}</span>`;
             if (data.runDistancePlan > 0) activityBubbles += `<span class="bubble run-plan">Run: ${data.runDistancePlan.toFixed(2)}km</span>`;
             if (data.bikeDistancePlan > 0) activityBubbles += `<span class="bubble bike-plan">Bike: ${data.bikeDistancePlan.toFixed(2)}km</span>`;
